c68ed583196ec72d6efe878dc0957cb757fa56cc
shubham-hac <72845960+shubham-hac@users.noreply.github.com>
2024-07-09 09:42:50 +0530

    Flutter todo app final commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo/loginpage.dart';
 
 
+
 void main() async {
    WidgetsFlutterBinding.ensureInitialized();
    SharedPreferences prefs = await SharedPreferences.getInstance();
